65b777e8432039ff3096762e298bc7a793f444ed
Jonah Tarver <jonahdevine@gmail.com>
2021-01-04 14:51:14 -0800

    Added GET, POST, DELETE handlers

---
+++ src/store.js
@@ -0,0 +1,10 @@
+const bookmarks = [
+    {
+        title: 'Google',
+        url: 'http://www.google.com',
+        description: 'A search engine with imense power.',
+        rating: 4.0
+    }
+]
+
+module.exports = bookmarks
